b104050336c276f844637bd922796f52dacd096e
Hugo Avila <hugok2k@gmail.com>
2023-01-28 23:23:24 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full h-screen text-5xl font-mono font-bold m-0 p-0">
+      <h1>QR Generator</h1>
+    </div>
+  )
+}
+
+export default App
