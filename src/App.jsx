f520f4e89f0915b9a2fc2cc739ba07b1bdea3108
Hugo Avila <hugok2k@gmail.com>
2023-01-28 23:41:16 -0300

    Add qr image generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import QRCode from 'react-qr-code'
+import { useState } from 'react'
+
 function App() {
+  const [value, setValue] = useState('')
+  const [size, setSize] = useState(128)
+
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen text-5xl font-mono font-bold m-0 p-0">
-      <h1>QR Generator</h1>
+    <div className="flex flex-col justify-center items-center w-full h-screen text-5xl font-mono font-bold">
+      <h1 className="mb-8">QR Generator</h1>
+      <QRCode value={value} />
+      <input
+        className="text-xl border-solid border-2 border-sky-300 rounded-md my-8 outline-none p-2 text-center"
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
     </div>
   )
 }
