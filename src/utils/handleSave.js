0ef76e97f045a266c98e81f19e7d780831131034
Hugo Avila <hugok2k@gmail.com>
2025-05-08 12:58:31 -0300

    fix: update handleSave function to use html2canvas-pro and improve async handling

--- src/utils/handleSave.js
+++ src/utils/handleSave.js
@@ -1,12 +1,17 @@
-import html2canvas from 'html2canvas'
+import html2canvas from 'html2canvas-pro'
 
-export const handleSave = () => {
+export async function handleSave() {
   const input = document.querySelector('.qr-code')
-  html2canvas(input).then((canvas) => {
-    const imgData = canvas.toDataURL('image/png')
-    const link = document.createElement('a')
-    link.download = 'qr-code.png'
-    link.href = imgData
-    link.click()
+  const canvas = await html2canvas(input, {
+    backgroundColor: null,
+    logging: false,
+    useCORS: true,
+    scale: 2
   })
+  
+  const imgData = canvas.toDataURL('image/png')
+  const link = document.createElement('a')
+  link.download = 'qr-code.png'
+  link.href = imgData
+  link.click()
 }
