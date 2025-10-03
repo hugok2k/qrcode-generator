e99893943214d2c2e0764371e571d956664ab481
Hugo Avila <hugok2k@gmail.com>
2025-10-03 13:04:10 -0300

    feat: integrate translation for footer content to support multilingual display

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,21 @@
-import { GitHubSVG, LinkedInSVG } from './SVG'
+import { useTranslation } from "react-i18next"
+import { GitHubSVG, LinkedInSVG } from "./SVG"
 
 export default function Footer() {
+  const { t } = useTranslation()
+
   return (
     <footer className="flex flex-col justify-center items-center text-lg text-teal-900 min-h-36">
       <p className="flex gap-2">
-        Made with
+        {t("made_with")}
         <span role="img" aria-label="love" className="text-red-700">
           ❤️
         </span>
-        by
+        {t("by")}
         <a
           href="https://hugoavila.dev/"
           target="_blank"
+          rel="noopener noreferrer"
           className="text-orange-500 cursor-pointer hover:text-orange-700"
         >
           Hugo Avila
@@ -21,19 +25,23 @@ export default function Footer() {
         <a
           href="https://github.com/hugok2k"
           target="_blank"
+          rel="noopener noreferrer"
           className="flex justify-center items-center gap-2 hover:text-orange-500"
         >
           <GitHubSVG />
-          Github
+          {t("github")}
         </a>
         <a
           href="https://www.linkedin.com/in/devhugoavila/"
           target="_blank"
+          rel="noopener noreferrer"
           className="flex justify-center items-center gap-2 hover:text-orange-500"
         >
           <LinkedInSVG />
-          Linkedin
+          {t("linkedin")}
         </a>
+        {/** biome-ignore lint/a11y/useAltText: Esto solo se usa para estadísticas  */}
+        {/** biome-ignore lint/performance/noImgElement: Esto solo se usa para estadísticas */}
         <img
           decoding="async"
           loading="eager"
